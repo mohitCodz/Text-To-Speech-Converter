1691de0f99275cfb7928f4e5cfac0e9d32514af6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-25 02:32:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let speech = new SpeechSynthesisUtterance();
+let voices =[];
+let voicesSelect =  document.querySelector("select");
+window.speechSynthesis.onvoiceschanged = ()=>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((voices,i)=> (voicesSelect.options[i] = new option (voices.name, i)));
+};
 document.querySelector("button").addEventListener("click",()=>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
